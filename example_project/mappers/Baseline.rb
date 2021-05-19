8919fd2a7c8f30c39af002c74e3b4275e2f054bf
Joe Robertson <joseph.robertson@nrel.gov>
2021-05-18 18:08:55 -0700

    Remove setting of schedules path.

--- example_project/mappers/Baseline.rb
+++ example_project/mappers/Baseline.rb
@@ -527,10 +527,6 @@ module URBANopt
             rescue
             end
 
-            if args[:schedules_type] == 'user-specified'
-              args[:schedules_path] = feature.schedules_path
-            end
-
             args[:schedules_variation] = 'unit-to-unit'
             begin
               args[:schedules_variation] = feature.schedules_variation
