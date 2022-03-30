4a06a4b2a2996f4cb2fa4f59c85ad0754afd5e9f
Nathan Moore <nathan.moore@nrel.gov>
2022-03-30 15:28:21 -0600

    update copyright text & dates with openstudio rake task

--- example_project/mappers/FlexibleHotWater.rb
+++ example_project/mappers/FlexibleHotWater.rb
@@ -1,5 +1,5 @@
-#*********************************************************************************
-# URBANopt™, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC, and other
+# *********************************************************************************
+# URBANopt™, Copyright (c) 2019-2022, Alliance for Sustainable Energy, LLC, and other
 # contributors. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
@@ -26,7 +26,7 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
-#*********************************************************************************
+# *********************************************************************************
 
 require 'urbanopt/reporting'
 
