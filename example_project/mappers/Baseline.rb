dcd04b3d65e48af4217d656ad5212e9deb72f318
Joe Robertson <joseph.robertson@nrel.gov>
2022-04-04 16:38:09 -0700

    Test new number of occupants argument.

--- example_project/mappers/Baseline.rb
+++ example_project/mappers/Baseline.rb
@@ -375,7 +375,7 @@ module URBANopt
       def get_climate_zone_iecc(epw)
         headers = CSV.open(epw, 'r') { |csv| csv.first }
         wmo = headers[5]
-        zones_csv = File.join(File.dirname(__FILE__), '../resources/hpxml-measures/HPXMLtoOpenStudio/resources/data_climate_zones.csv')
+        zones_csv = File.join(File.dirname(__FILE__), '../resources/hpxml-measures/HPXMLtoOpenStudio/resources/data/climate_zones.csv')
         CSV.foreach(zones_csv) do |row|
           if row[0].to_s == wmo.to_s
             return row[6].to_s
@@ -512,6 +512,12 @@ module URBANopt
             args[:geometry_unit_cfa] = feature.floor_area / args[:geometry_building_num_units]
 
             args[:geometry_unit_num_bedrooms] = feature.number_of_bedrooms / args[:geometry_building_num_units]
+
+            args[:geometry_unit_num_occupants] = 'auto'
+            begin
+              args[:geometry_unit_num_occupants] = "#{feature.number_of_occupants / args[:geometry_building_num_units]}"
+            rescue
+            end
 
             args[:geometry_average_ceiling_height] = 8.0
             begin
