38883f3f28714fc5830a5edf58fd880199c072fb
kflemin <2205659+kflemin@users.noreply.github.com>
2022-07-06 14:32:12 -0600

    store coverage results

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,27 +39,12 @@
 # *********************************************************************************
 
 require 'simplecov'
+require 'simplecov-lcov'
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov::Formatter::LcovFormatter.config.single_report_path = 'coverage/lcov.info'
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.start
 
-SimpleCov.start 'rails' do
-  if ENV['CI']
-    require 'simplecov-lcov'
-
-    SimpleCov::Formatter::LcovFormatter.config do |c|
-      c.report_with_single_file = true
-      c.single_report_path = 'coverage/lcov.info'
-    end
-
-    formatter SimpleCov::Formatter::LcovFormatter
-  end
-
-  add_filter %w[version.rb initializer.rb]
-end
-
-# unless ENV['SKIP_COVERALLS']
-#   require 'coveralls'
-#   Coveralls.wear!
-#   #SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-# end
 
 require 'bundler/setup'
 
