4f42cba3e1ba87be956ffe48d270fe190d63546f
Nathan Moore <nathan.moore@nrel.gov>
2022-07-11 09:04:07 -0600

    make rubocop happier

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,7 +44,7 @@ require 'simplecov-lcov'
 SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start do
-  add_filter "/spec/"
+  add_filter '/spec/'
 end
 
 require 'bundler/setup'
